16784409a53b12fa4df2a53de65e4613a0c0c678
Alessio Pampaloni <pampalonialessio2008@gmail.com>
2026-06-24 10:50:32 +0200

    Restyle media widget as system surface

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
     applicationId = "com.pampa.widgets"
     minSdk = 31
     targetSdk = 36
-    versionCode = 3
-    versionName = "0.2.1"
+    versionCode = 4
+    versionName = "0.2.2"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
